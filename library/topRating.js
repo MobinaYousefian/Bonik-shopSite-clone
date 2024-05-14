41371a731d1e2f227d857cf79fcb0619c65b53a7
Mobina <mobina.ysf203@gmail.com>
2024-05-15 03:12:09 +0330

    add (): added data, assets, theme

---
+++ library/topRating.js
@@ -0,0 +1,11 @@
+import Image1 from "../public/asset/topRating/camera.png";
+import Image2 from "../public/asset/topRating/mobile.png";
+import Image3 from "../public/asset/topRating/shoes.png";
+import Image4 from "../public/asset/topRating/watch.png";
+
+export const topRating = [
+    {id:1, image:Image1, rating:4, title:'Camera', total:49, price:'$3300'},
+    {id:2, image:Image2, rating:4.5, title:'Shoe', total:20, price:'$400'},
+    {id:3, image:Image3, rating:5, title:'Phone', total:65, price:'$999'},
+    {id:4, image:Image4, rating:5, title:'Watch', total:75, price:'$999'},
+]
