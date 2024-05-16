acbf5c0c8ec458c59cffdb85d15f3210e205c0be
Mobina <mobina.ysf203@gmail.com>
2024-05-16 18:50:29 +0330

    fix (Brands): fixed responsive css for tablet breakpoint

--- section/home/brands/Brands.js
+++ section/home/brands/Brands.js
@@ -8,17 +8,17 @@ const Brands = ({category , title, display}) => {
     return (
         <Grid container spacing={3} my={3}>
             <Grid
-                item xs={0} md={3}
+                item xs={0} sm={0} md={3}
                 display={"none"}
                 sx={{
-                    '@media (min-width: 900px)': {
+                    '@media (min-width: 600px)': {
                         display : "block"
                     },
                 }}
             >
                 <Brand display={display}/>
             </Grid>
-            <Grid item xs={12} md={9} pl={3}>
+            <Grid item xs={12} sm={7} md={9} pl={3}>
                 <Title title={title} icon={''}/>
                 <Grid container spacing={3}>
                     {ProductData.filter(p=> p.category === category).map(product => (
