1394493d973cad771c06818401dd84e9ba08b6f6
Mobina <mobina.ysf203@gmail.com>
2024-05-22 23:44:05 +0330

    add (SearchInput): added search query onclick for filters

--- library/checks.js
+++ library/checks.js
@@ -2,49 +2,49 @@ import {Rating} from "@mui/lab";
 
 export const check1 = [
     {
-        id:1,
+        id:11,
         title:'Maccs'
     },
     {
-        id:2,
+        id:22,
         title:'Karts'
     },
     {
-        id:3,
+        id:33,
         title:'Baars'
     },
     {
-        id:4,
+        id:44,
         title:'Bukks'
     },
     {
-        id:5,
+        id:55,
         title:'Luasis'
     }
 ]
 
 export const check2 = [
     {
-        id:1,
+        id:11,
         title:'On Sale'
     },
     {
-        id:2,
+        id:22,
         title:'In Stock'
     },
     {
-        id:3,
+        id:33,
         title:'Featured'
     }
 ]
 
 export const check3 = [
     {
-        id:1,
+        id:5,
         title:<Rating size={'small'} name="read-only" value={5} readOnly />
     },
     {
-        id:2,
+        id:4,
         title:<Rating size={'small'} name="read-only" value={4} readOnly />
     },
     {
@@ -52,11 +52,11 @@ export const check3 = [
         title:<Rating size={'small'} name="read-only" value={3} readOnly />
     },
     {
-        id:4,
+        id:2,
         title:<Rating size={'small'} name="read-only" value={2} readOnly />
     },
     {
-        id:5,
+        id:1,
         title:<Rating size={'small'} name="read-only" value={1} readOnly />
     }
 ]
--- section/search/SearchInput.js
+++ section/search/SearchInput.js
@@ -1,9 +1,67 @@
+'use client'
+import {usePathname, useRouter, useSearchParams} from "next/navigation";
+
+const status = ['On Sale', 'In Stock', 'Featured'];
+
 const SearchInput = ({option}) => {
+    const router = useRouter();
+    const searchParams = useSearchParams();
+    const pathName = usePathname();
+
+    const queriesArray = Array.from(searchParams.values());
+
+
+    const handleRatingQuery = (id) => {
+        if (searchParams.has("q")) {
+            if (searchParams.has("rating")) {
+                const urlSearchParams = new URLSearchParams(searchParams);
+                urlSearchParams.delete("rating");
+                urlSearchParams.append("rating", id);
+                router.push(`${pathName}?${urlSearchParams}`);
+            }else {
+                const urlSearchParams = new URLSearchParams(searchParams);
+                urlSearchParams.append("rating", id);
+                router.push(`${pathName}?${urlSearchParams}`);
+            }
+        }else {
+            const urlSearchParams = new URLSearchParams(searchParams);
+            urlSearchParams.set("rating", id);
+            router.push(`${pathName}?${urlSearchParams}`);
+        }
+    };
+
+    const handleStringQuery = (title) => {
+        const hasStatus = status.includes(title);
+
+        if (  hasStatus || (hasStatus && searchParams.has("status")) ) {
+            const urlSearchParams = new URLSearchParams(searchParams);
+            urlSearchParams.set("status", title);
+            router.push(`${pathName}?${urlSearchParams}`);
+        }else if ( hasStatus && searchParams.has("q") ) {
+            const urlSearchParams = new URLSearchParams(searchParams);
+            urlSearchParams.append("status", title);
+            router.push(`${pathName}?${urlSearchParams}`);
+        }else {
+            const urlSearchParams = new URLSearchParams(searchParams);
+            urlSearchParams.set("q", title);
+            router.push(`${pathName}?${urlSearchParams}`);
+        }
+    };
+
+    const handleQueryParam = (title, id) => {
+        if (typeof title === "object") {
+            handleRatingQuery(id)
+        }else {
+            handleStringQuery(title)
+        }
+    };
+
     return(
         <>
             {option.map(o => (
-                <label key={o.id} className="container">{o.title}
-                    <input type="checkbox" name="radio"/>
+                <label onClick={() => handleQueryParam(o.title, o.id)} key={o.id} className="container">
+                    {o.title}
+                    <input type="checkbox" checked={queriesArray.includes(o.title) || queriesArray.includes(`${o.id}`)} name="radio" readOnly />
                     <span className="checkmark"> </span>
                 </label>
             ))}
