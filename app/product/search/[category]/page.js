8bc7b0004c32959c17a1bcd31617543698bc7318
Mobina <mobina.ysf203@gmail.com>
2024-05-25 21:10:58 +0330

    fix ([category] page): fixed deploy bug

--- app/product/search/[category]/page.js
+++ app/product/search/[category]/page.js
@@ -1,12 +1,15 @@
 import {ProductData} from "@/library/ProductData";
 import SearchCategoryPage from "@/components/SearchCategoryPage";
+import {Suspense} from "react";
 
 export default function SearchCategory({params}) {
     const category = params.category;
     const categoryData = ProductData.filter(p => p.category === category);
 
     return (
-        <SearchCategoryPage categoryData={categoryData}/>
+        <Suspense fallback={"loading"}>
+            <SearchCategoryPage categoryData={categoryData}/>
+        </Suspense>
      )
 }
 
